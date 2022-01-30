0ef17250131d9e7aa16aa5cb525e1621eff6b651
shoppingzh <1049262057@qq.com>
2022-01-30 22:43:14 +0800

    katex demo

---
+++ katex/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div ref="el" />
+</template>
+
+<script setup>
+import katex from 'katex'
+import 'katex/dist/katex.css'
+import { onMounted, ref } from 'vue'
+const el = ref(null)
+onMounted(() => {
+  katex.render('\\sqrt{2}', el.value, {
+    // displayMode: true,
+    output: 'html'
+  })
+})
+</script>
+
+<style>
+</style>
