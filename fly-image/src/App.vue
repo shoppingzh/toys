62e80d7dd2a5e4619aba08ff623f23ff81e6ca01
shoppingzh <1049262057@qq.com>
2022-02-04 23:09:57 +0800

    add fly-image demo

---
+++ fly-image/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import FlyImage from 'fly-image'
+
+const img = ref(null)
+onMounted(() => {
+  new FlyImage(img.value)
+})
+</script>
+
+<template>
+  <img ref="img" src="./assets/1.jpg" width="500">
+</template>
+
+<style>
+</style>
