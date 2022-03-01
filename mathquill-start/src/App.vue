b366da17d037ddcd8477bcc1be4022d6413eba9a
shoppingzh <1049262057@qq.com>
2022-03-01 15:32:24 +0800

    mathquill demo

---
+++ mathquill-start/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, onMounted } from "vue";
+
+const input = ref(null)
+onMounted(() => {
+  const MQ = MathQuill.getInterface(2)
+  const mathField = MQ.MathField(input.value, {
+
+  })
+  console.log(mathField)
+})
+</script>
+
+<template>
+  <div ref="input" class="input"></div>
+</template>
+
+<style>
+  .input {
+    width: 100%;
+    height: 300px;
+    border: 1px solid rgba(0, 0, 0, .4);
+    outline: none;
+  }
+</style>
