5e90caf3fef777f8400e5058ef353e225fa224a7
郑晓平 <zhengxiaoping172@sf-express.com>
2022-07-07 18:38:42 +0800

    add MutationObserver demo

---
+++ learn-mutation-observer/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div ref="el" style="width: 300px; height: 150px; background-color: orange;" />
+</template>
+
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+const el = ref<HTMLElement>(null)
+
+onMounted(() => {
+  new MutationObserver((mutations, observer) => {
+    console.log(mutations)
+  }).observe(el.value, {
+    attributes: true,
+    attributeOldValue: true,
+    childList: true,
+    subtree: true
+  })
+
+  new MutationObserver((mutations) => {
+    console.log('parent mutations: ')
+    console.log(mutations)
+  }).observe(el.value.parentNode, {
+    childList: true,
+    subtree: true
+  })
+
+})
+</script>
