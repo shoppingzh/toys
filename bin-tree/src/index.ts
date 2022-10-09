20cb49e4e0bfcfdd4daa115b770bf96990448746
shoppingzh <xpzheng6@163.com>
2022-10-09 16:58:05 +0800

    update

--- bin-tree/src/index.ts
+++ bin-tree/src/index.ts
@@ -1,48 +1,4 @@
-interface TreeNode {
-  val: number
-  left?: TreeNode
-  right?: TreeNode
-}
-
-const root: TreeNode = {
-  val: 1,
-  left: {
-    val: 2,
-    left: {
-      val: 4,
-      left: {
-        val: 8
-      },
-      right: {
-        val: 9
-      }
-    },
-    right: {
-      val: 5
-    }
-  },
-  right: {
-    val: 3,
-    left: {
-      val: 6,
-      left: {
-        val: 10
-      },
-      right: {
-        val: 11,
-        left: {
-          val: 12
-        },
-        right: {
-          val: 13
-        }
-      }
-    },
-    right: {
-      val: 7
-    }
-  }
-}
+import root, { TreeNode } from './data'
 
 function iterate(root: TreeNode) {
   const queue: TreeNode[] = [root]
