b3ca0d73241f7ff266973ef6afa63777fe61eeb4
shoppingzh <xpzheng6@163.com>
2022-12-14 20:11:53 +0800

    update

---
+++ learn-tailwindcss/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- <div class="w-32 h-32 bg-blue-500">
+
+  </div> -->
+
+  <div class="p-2 text-gray-900 font-semibold">首页</div>
+  <div class="p-2 text-gray-900 font-semibold">学习TailwindCSS</div>
+  <div class="p-2 text-gray-900 font-semibold">TailwindCSS的设计哲学</div>
+  <div class="p-2 text-gray-900 font-semibold">最佳实践</div>
+
+  <div class="menu">首页</div>
+  <div class="menu">学习TailwindCSS</div>
+  <div class="menu">TailwindCSS的设计哲学</div>
+  <div class="menu">最佳实践</div>
+
+  <div
+    v-for="menu in ['首页', '学习TailwindCSS', 'TailwindCSS的设计哲学', '最佳实践']"
+    :key="menu"
+    class="p-2 text-gray-900 font-semibold">{{ menu }}</div>
+
+  <div class="div" />
+
+  <div class="hello w-32 h-32 bg-blue-500" />
+</template>
+
+<style>
+.menu {
+  @apply p-2 text-gray-900 font-semibold;
+}
+
+.div {
+  border-top: 1px solid theme('colors.gray.200');
+}
+
+@media (max-width: 768px) {
+  .hello {
+    @apply bg-green-500;
+  }
+}
+
+</style>
